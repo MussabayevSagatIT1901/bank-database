6358ef154127199ffee213a877cd102b5844ff90
MussabayevSagatIT1901 <57889413+MussabayevSagatIT1901@users.noreply.github.com>
2021-06-03 13:22:46 +0600

    Create products.sql

---
+++ products.sql
@@ -0,0 +1,16 @@
+INSERT INTO "products" VALUES
+    (1,'mortgage',' a way to use one''s real property as a
+ guarantee for a loan to get money.'),
+    (2,'cash loan','a type of debt financing, in which a bank 
+lends funds, generally for working capital, 
+using the expected cash flows that a 
+borrowing company generates as collateral 
+for the loan.'),
+    (3,'car loan','a sum of money a consumer borrows in
+ order to purchase a car'),
+    (4,'credit car','a payment card issued to users 
+(cardholders) to enable the cardholder
+ to pay a merchant for goods and services
+ based on the cardholder''s promise to
+ the card issuer to pay them for the 
+amounts plus the other agreed charges');
