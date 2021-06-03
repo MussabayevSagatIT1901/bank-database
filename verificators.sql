288c4514716e7a69182632f08f0ebedd308d09c3
MussabayevSagatIT1901 <57889413+MussabayevSagatIT1901@users.noreply.github.com>
2021-06-03 13:18:31 +0600

    Create verificators.sql

---
+++ verificators.sql
@@ -0,0 +1,8 @@
+INSERT INTO "verificators" VALUES
+    (1,'2007-02-01 00:00:00','true','Francis Bowman'),
+    (2,'2009-01-09 00:00:00','false','Laurence Edwards'),
+    (3,'2010-11-15 00:00:00','false','Darryl George'),
+    (4,'2011-05-23 00:00:00','true','Roman Tran'),
+    (5,'2012-06-06 00:00:00','true','Marcos Harrison');
+
+	
